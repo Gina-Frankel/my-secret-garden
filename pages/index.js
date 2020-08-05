2699c593738bbeb69539bc0315e96a7a29feb290
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 09:56:32 +0100

    Create Next.js App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head></Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
