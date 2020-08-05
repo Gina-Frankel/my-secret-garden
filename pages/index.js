5055b593dfadcafa58fb74df027a695aa3db65f2
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 11:47:24 +0100

    Add Header with text + basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { Header } from "semantic-ui-react";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}> </main>
+    <div>
+      <main className={styles.main}>
+        {" "}
+        <Header size="large" color="teal">
+          Discover a garden near you
+        </Header>{" "}
+      </main>
     </div>
   );
 }
