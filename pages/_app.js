b3ade1af7a1732c278e6aa06662539c99803e448
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 17:15:58 +0100

    Call  function on components + if function exists pass pageProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import "../styles/globals.css";
 import Layout from "../components/_App/Layout";
+// import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
+import App from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
-  );
+class MyApp extends App {
+  static async GetInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.GetInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps: pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    );
+  }
 }
 
 export default MyApp;
