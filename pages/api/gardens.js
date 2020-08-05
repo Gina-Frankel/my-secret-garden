78adcdd7cb4cfd76100f7f18bbfac9aa27909620
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 20:10:48 +0100

    Find all gardens in database

--- pages/api/gardens.js
+++ pages/api/gardens.js
@@ -2,11 +2,12 @@
 // routes are simple functions
 // next makes it very easy to make routes - just need a function - you do not need configuration
 // do not have to import node it is already available
-
+import garden from "../../models/garden";
 import connectDb from "../../utils/connectDb";
 
 connectDb();
 
-export default (req, res) => {
-  res.status(200).send("test");
+export default async (req, res) => {
+  const gardens = await garden.find();
+  res.status(200).json(gardens);
 };
