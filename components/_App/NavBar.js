86f3c29abd9bb5ebb1cc23fb44f0d2f3e583b21e
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 11:37:36 +0100

    Return Icon in NavBar component

--- components/_App/NavBar.js
+++ components/_App/NavBar.js
@@ -1,12 +1,15 @@
-import { Menu, Container } from "semantic-ui-react";
+import { Menu, Container, Icon } from "semantic-ui-react";
 import Link from "next/link";
 
 function NavBar() {
   return (
-    <Menu stackable fluid={true} id="menu" inverted={true}>
-      <Container>
+    <Menu>
+      <Container text>
         <Link href="/">
-          <Menu.Item header>My secret garden</Menu.Item>
+          <Menu.Item header>
+            <Icon name="tree" size="large" />
+            My secret garden
+          </Menu.Item>
         </Link>
       </Container>
     </Menu>
