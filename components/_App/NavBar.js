4fd93be9d3ccee6832e8c417745b25411e1bc2d8
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 11:27:24 +0100

    Add Link and Menu Item with text to NavBar

--- components/_App/NavBar.js
+++ components/_App/NavBar.js
@@ -1,5 +1,16 @@
+import { Menu, Container } from "semantic-ui-react";
+import Link from "next/link";
+
 function NavBar() {
-  return <div>navbar</div>;
+  return (
+    <Menu stackable fluid={true} id="menu" inverted={true}>
+      <Container>
+        <Link href="/">
+          <Menu.Item header>My secret garden</Menu.Item>
+        </Link>
+      </Container>
+    </Menu>
+  );
 }
 
 export default NavBar;
