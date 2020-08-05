dd64cd215c23b679c38437621af7bc85842336e8
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 10:33:34 +0100

    Remove Head component

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head></Head>
-
-      <main className={styles.main}></main>
+      <main className={styles.main}> </main>
 
       <footer className={styles.footer}></footer>
     </div>
