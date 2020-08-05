e3b0faa277c0a6e4e6b2d1953a4739e741ff28fb
Gina Frankel <g.frankel22@gmail.com>
2020-08-05 17:52:24 +0100

    Get  response from gardens api

--- pages/gardens.js
+++ pages/gardens.js
@@ -1,13 +1,17 @@
 // where the gardens from mongo go
 // make a request to mongo from here
 
+import Axios from "axios";
+
 function Gardens(props) {
-  console.log(props);
+  console.log(props.gardens);
   return <div>gardens</div>;
 }
 
-Gardens.getInitialProps = () => {
-  return { Hello: "world" };
+Gardens.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/gardens";
+  const response = await Axios.get(url);
+  return { gardens: response.data };
   //fetch data on server to google
   //return response data as an object
 };
