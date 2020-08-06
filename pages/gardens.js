08627bca99e892f8db8a290dee90bfd99bd08683
Gina Frankel <g.frankel22@gmail.com>
2020-08-06 10:59:01 +0100

    Add GardenList component to return

--- pages/gardens.js
+++ pages/gardens.js
@@ -1,17 +1,22 @@
-// where the gardens from mongo go
-// make a request to mongo from here
-
-import Axios from "axios";
+import axios from "axios";
+import GardenList from "../components/garden/GardenList";
 
 function Gardens(props) {
-  console.log(props.gardens);
-  return <div>gardens</div>;
+  // console.log(props.gardens);
+  return (
+    <div>
+      <GardenList gardens={props}></GardenList>
+    </div>
+  );
 }
 
+// make a request to mongo from here
 Gardens.getInitialProps = async () => {
   const url = "http://localhost:3000/api/gardens";
-  const response = await Axios.get(url);
+  const response = await axios.get(url);
+
   return { gardens: response.data };
+
   //fetch data on server to google
   //return response data as an object
 };
