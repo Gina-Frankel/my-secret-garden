319d77f24f4713d45512545f236c2dfa2b356506
Gina Frankel <g.frankel22@gmail.com>
2020-08-08 11:42:07 +0100

    Add address to Card description

--- components/garden/GardenList.js
+++ components/garden/GardenList.js
@@ -1,8 +1,10 @@
 import { Card } from "semantic-ui-react";
 
 function GardenList(props) {
+  console.log(props.gardens.gardens);
   const items = props.gardens.gardens.map((item) => ({
     header: item.name,
+    description: item.address,
     color: "teal",
     childKey: item._id,
   }));
