66e22ffbe22d62f2d955653b81c309b865ffae74
Gina Frankel <g.frankel22@gmail.com>
2020-08-10 12:42:46 +0100

    Add function to decode data +  use  in map of gardens so image is displayed

--- components/garden/GardenList.js
+++ components/garden/GardenList.js
@@ -1,13 +1,18 @@
 import { Card } from "semantic-ui-react";
 
 function GardenList(props) {
-  console.log(props.gardens.gardens);
-  const items = props.gardens.gardens.map((item) => ({
-    header: item.name,
-    description: item.address,
-    color: "teal",
-    childKey: item._id,
-  }));
+  const decodeData = (data) => window.atob(data);
+
+  const items = props.gardens.gardens.map((item) => {
+    const data_URI = "data:image/jpeg;base64,";
+    return {
+      header: item.name,
+      description: item.address,
+      color: "teal",
+      childKey: item._id,
+      image: data_URI + decodeData(item.data),
+    };
+  });
 
   return (
     <>
