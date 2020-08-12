f79f1dec35aa80a4ff73a93985006112b11a527f
Gina Frankel <g.frankel22@gmail.com>
2020-08-12 16:39:24 +0100

    Change Link href + Button text

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Header, Button } from "semantic-ui-react";
 import Link from "next/link";
+
 //import React from "react";
 // import Map from "../components/Map";
 
@@ -17,8 +18,8 @@ function Home() {
         <Link href="/gardens?_area=north-london">
           <Button> Near North London</Button>
         </Link>
-        <Link href="/gardens?_area=city">
-          <Button> Near the City</Button>
+        <Link href="/gardens?_area=south-east-london">
+          <Button> Near South East London</Button>
         </Link>
       </main>
     </div>
