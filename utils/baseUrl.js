a4535f1278cc2ad94c9cbbefe2b42e504b14fc6c
Gina Frankel <g.frankel22@gmail.com>
2020-08-12 17:47:31 +0100

    Fix prod base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,7 @@
 // built in to node
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "my-secret-garden-dun.vercel.app"
+    ? "http://my-secret-garden-dun.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
