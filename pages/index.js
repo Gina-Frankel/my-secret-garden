89121fc66127e5275a13a9942ad2e39015e0e46b
Gina Frankel <g.frankel22@gmail.com>
2020-08-12 22:20:37 +0100

    Refactor Links/Buttons by mapping list of ButtonLinks

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Header, Button } from "semantic-ui-react";
 import Link from "next/link";
 
-//import React from "react";
-// import Map from "../components/Map";
-
 function Home() {
+  const ButtonLinks = [
+    {
+      name: "Near North London",
+      href: "/gardens?_area=north-london",
+    },
+    {
+      name: "Near London City",
+      href: "/gardens?_area=city-london",
+    },
+    {
+      name: "Near South East London",
+      href: "/gardens?_area=south-east-london",
+    },
+  ];
+  const ButtonLink = ButtonLinks.map((linkItem) => (
+    <Link href={linkItem.href}>
+      <Button basic color="teal">
+        {linkItem.name}
+      </Button>
+    </Link>
+  ));
+
   return (
     <div>
       <main className={styles.main}>
@@ -15,21 +33,7 @@ function Home() {
           My Secret Garden
           <Header.Subheader> Discover gardens near you </Header.Subheader>
         </Header>
-        <Button.Group>
-          <Link href="/gardens?_area=north-london">
-            <Button basic color="teal">
-              Near North London
-            </Button>
-          </Link>
-          <Link href="/gardens?_area=south-east-london">
-            <Button color="teal">Near South East London</Button>
-          </Link>
-          <Link href="/gardens?_area=city-london">
-            <Button basic color="teal">
-              Near city of London
-            </Button>
-          </Link>
-        </Button.Group>
+        <Button.Group>{ButtonLink}</Button.Group>
       </main>
     </div>
   );
